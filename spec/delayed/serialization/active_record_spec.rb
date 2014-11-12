3c38e2d305b7847eaf7ccf0a1307e74ec243131b
Joel Turkel <jturkel@salsify.com>
2014-11-12 09:40:03 -0500

    Test for https://github.com/collectiveidea/delayed_job/issues/740

--- spec/delayed/serialization/active_record_spec.rb
+++ spec/delayed/serialization/active_record_spec.rb
@@ -3,13 +3,34 @@ require "helper"
 describe ActiveRecord do
   it "loads classes with non-default primary key" do
     expect do
-      YAML.load(Story.create.to_yaml)
+      load_yaml(Story.create.to_yaml)
     end.not_to raise_error
   end
 
   it "loads classes even if not in default scope" do
     expect do
-      YAML.load(Story.create(scoped: false).to_yaml)
+      load_yaml(Story.create(scoped: false).to_yaml)
     end.not_to raise_error
+  end
+
+  it "loads embedded classes even if not in default scope" do
+    job = StoryJob.new(Story.create(scoped: false))
+    expect do
+      load_yaml(job.to_yaml)
+    end.not_to raise_error
+  end
+
+  def load_yaml(yaml)
+    if YAML.respond_to?(:load_dj)
+      YAML.load_dj(yaml)
+    else
+      YAML.load(yaml)
+    end
+  end
+
+  StoryJob = Struct.new(:story) do
+    def perform
+
+    end
   end
 end
